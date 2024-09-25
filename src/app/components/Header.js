36bc9f0704d19e76ed24b1f1cd910461431f586c
Riya <riyashivolkar2@gmail.com>
2024-09-25 16:16:05 +0530

    nav-bar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -59,36 +59,35 @@ const Header = () => {
 
       {/* Mobile navigation menu */}
       <div
-        className={`fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 transition-transform duration-300 ease-in-out ${
+        className={`fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50 transition-transform duration-300 ease-in-out ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         } md:hidden`}
       >
-        <div className="relative flex flex-col items-center justify-center w-4/5 max-w-md p-2 mx-20 transition-all duration-300 bg-gray-200 border-2 rounded-lg shadow-lg sm:p-6">
-          <div className="flex items-center justify-end w-full ">
-            {/* <h2 className="font-bold text-black sm:text-3xl text-md">
-               Menu
-            </h2> */}
+        <div className="relative flex flex-col items-center justify-center w-4/5 max-w-md p-8 mx-20 transition-all duration-500 transform rounded-lg shadow-xl bg-gradient-to-br from-white to-gray-100">
+          <div className="flex items-center justify-between w-full mb-6">
+            <h2 className="text-3xl font-bold text-gray-800">Menu</h2>
             <button
               onClick={handleMenuToggle}
-              className="text-gray-600 transition-opacity hover:text-black"
+              className="text-gray-500 transition-opacity hover:text-gray-800"
               aria-label="Close menu"
             >
               <Image
                 src="/close.svg"
-                className="text-orange-400 "
+                className="hover:text-orange-400"
                 alt="Close"
-                width={20}
-                height={20}
+                width={30}
+                height={30}
               />
             </button>
           </div>
-          <nav className="flex flex-col items-center w-[75%] space-y-3">
+
+          <nav className="flex flex-col items-center w-full space-y-4">
             {menuHeader.map((item, index) =>
               item.path ? (
                 <Link
                   key={index}
                   href={item.path}
-                  className="w-full py-2 text-sm text-center text-black transition bg-white rounded-md hover:bg-gray-200"
+                  className="w-full py-3 text-lg text-center text-black transition-all duration-300 transform bg-white rounded-lg shadow-md hover:bg-orange-400 hover:text-white hover:shadow-lg hover:scale-105"
                   onClick={() => setIsMenuOpen(false)}
                 >
                   {item.name}
