76c9ac61da43366e1844449ea94dc28aefcb1b8d
Riya <riyashivolkar2@gmail.com>
2024-09-26 06:33:03 +0530

    padding

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -8,40 +8,40 @@ const page = () => {
           <h1 className=" sm:text-4xl text-xl font-medium title-font mb-4  text-[rgb(247,148,29)] ">
             Contact Us
           </h1>
-          <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <div className="relative flex items-end justify-start p-10 overflow-hidden bg-gray-300 rounded-lg lg:w-2/3 md:w-1/2 sm:mr-10">
             <iframe
               width="100%"
               height="100%"
-              class="absolute inset-0"
-              frameborder="0"
+              className="absolute inset-0"
+              frameBorder="0"
               title="map"
-              marginheight="0"
-              marginwidth="0"
+              marginHeight="0"
+              marginWidth="0"
               scrolling="no"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62332.94803706223!2d73.8133453!3d15.5940644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfeae1e59582c9%3A0xabd8701669ec1f3c!2sSaldanha%20Towers%2C%20Duler-Marna-Siolim%20Rd%2C%20Dangui%20Colony%2C%20Mapusa%2C%20Goa%20403507!5e0!3m2!1sen!2sin!4v1695070669925!5m2!1sen!2sin"
               style={{ filter: "grayscale(0.8) contrast(1.2) opacity(0.6)" }}
             ></iframe>
-            <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-              <div class="lg:w-1/2 px-6">
-                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+            <div className="relative flex flex-wrap py-6 bg-white rounded shadow-md">
+              <div className="px-6 lg:w-1/2">
+                <h2 className="text-xs font-semibold tracking-widest text-gray-900 title-font">
                   ADDRESS
                 </h2>
-                <p class="mt-1 text-xs my-2">
+                <p className="py-1 mt-1 text-xs">
                   B 403 4th Floor, Saldanha Business tower, Near court junction,
                   Mapusa, Bardez, Goa 403507
                 </p>
               </div>
-              <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+              <div className="px-6 mt-4 lg:w-1/2 lg:mt-0">
+                <h2 className="text-xs font-semibold tracking-widest text-gray-900 title-font">
                   EMAIL
                 </h2>
-                <a class="text-orange-400 text-xs leading-relaxed">
+                <a className="text-xs leading-relaxed text-orange-400">
                   buildmydocuments@email.com
                 </a>
-                <h2 class="title-font uppercase font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                <h2 className="mt-4 text-xs font-semibold tracking-widest text-gray-900 uppercase title-font">
                   Business Hours
                 </h2>
-                <p class="leading-relaxed text-xs my-2">
+                <p className="py-1 text-xs leading-relaxed">
                   Monday to Friday (09:30-17:30)
                 </p>
               </div>
@@ -49,49 +49,49 @@ const page = () => {
           </div>
 
           {/* //Feedback-form */}
-          {/* <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+          {/* <div className="flex flex-col w-full mt-8 bg-white lg:w-1/3 md:w-1/2 md:ml-auto md:py-8 md:mt-0">
+            <h2 className="mb-1 text-lg font-medium text-gray-900 title-font">
               Feedback
             </h2>
-            <p class="leading-relaxed mb-5 text-gray-600">
+            <p className="mb-5 leading-relaxed text-gray-600">
               Post-ironic portland shabby chic echo park, banjo fashion axe
             </p>
-            <div class="relative mb-4">
-              <label for="name" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label for="name" className="text-sm leading-7 text-gray-600">
                 Name
               </label>
               <input
                 type="text"
                 id="name"
                 name="name"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
               />
             </div>
-            <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label for="email" className="text-sm leading-7 text-gray-600">
                 Email
               </label>
               <input
                 type="email"
                 id="email"
                 name="email"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
               />
             </div>
-            <div class="relative mb-4">
-              <label for="message" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label for="message" className="text-sm leading-7 text-gray-600">
                 Message
               </label>
               <textarea
                 id="message"
                 name="message"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
               ></textarea>
             </div>
-            <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button className="px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
               Button
             </button>
-            <p class="text-xs text-gray-500 mt-3">
+            <p className="mt-3 text-xs text-gray-500">
               Chicharrones blog helvetica normcore iceland tousled brook viral
               artisan.
             </p>
