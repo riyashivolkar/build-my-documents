f650c660d6baf2f2a7ed1ae13c5d13b50d65af48
Riya <64467020+riyashivolkar@users.noreply.github.com>
2024-11-11 06:44:14 +0530

    use client-search-param

--- src/app/talk-to-a-lawyer/booking/components/FormBooking.js
+++ src/app/talk-to-a-lawyer/booking/components/FormBooking.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { timeSlots, icons } from "./dataBooking";
 import { useSearchParams, useRouter } from "next/navigation"; // Use next/navigation
