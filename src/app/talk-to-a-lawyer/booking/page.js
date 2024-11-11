ea71a3e975de087301cd7c6edc79e1f5a54801a5
Riya <64467020+riyashivolkar@users.noreply.github.com>
2024-11-11 07:11:57 +0530

    sus

--- src/app/talk-to-a-lawyer/booking/page.js
+++ src/app/talk-to-a-lawyer/booking/page.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import BookingSteps from "./components/BookingSteps";
 import FormBooking from "./components/FormBooking";
 
@@ -22,29 +22,31 @@ const BookingPage = () => {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
-      <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
-        <div className="bg-white ">
-          <Link href="/">
-            <Image
-              src="/logo1.png"
-              alt="logo"
-              width={280}
-              height={68}
-              priority
-              className="w-40 h-auto sm:w-80 md:h-auto"
-            />
-          </Link>
-        </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
+        <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
+          <div className="bg-white ">
+            <Link href="/">
+              <Image
+                src="/logo1.png"
+                alt="logo"
+                width={280}
+                height={68}
+                priority
+                className="w-40 h-auto sm:w-80 md:h-auto"
+              />
+            </Link>
+          </div>
 
-        <div className="bg-gray-100 ">
-          <BookingSteps />
-        </div>
-        <div className="">
-          <FormBooking />
+          <div className="bg-gray-100 ">
+            <BookingSteps />
+          </div>
+          <div className="">
+            <FormBooking />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
