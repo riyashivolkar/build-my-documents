74e18d8497358d3b4b5c6bb6bb094e4d429581a6
Riya <64467020+riyashivolkar@users.noreply.github.com>
2024-11-11 07:25:10 +0530

    sus3

--- src/app/talk-to-a-lawyer/booking/step2/page.js
+++ src/app/talk-to-a-lawyer/booking/step2/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { db } from "../../../../../firebase/firebaseConfig"; // Ensure correct import paths
 import { doc, getDoc, updateDoc } from "firebase/firestore";
@@ -44,31 +44,33 @@ const Step2 = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
-        <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
-          <div className="bg-white ">
-            <Link href="/">
-              <Image
-                src="/logo1.png"
-                alt="logo"
-                width={280}
-                height={68}
-                priority
-                className="w-40 h-auto sm:w-80 md:h-auto"
-              />
-            </Link>
-          </div>
+    <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
+      <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
+        <div className="bg-white ">
+          <Link href="/">
+            <Image
+              src="/logo1.png"
+              alt="logo"
+              width={280}
+              height={68}
+              priority
+              className="w-40 h-auto sm:w-80 md:h-auto"
+            />
+          </Link>
+        </div>
 
-          <div className="bg-gray-100 ">
+        <div className="bg-gray-100 ">
+          <Suspense fallback={<div>Loading...</div>}>
             <StepStep2 />
-          </div>
-          <div className="">
+          </Suspense>
+        </div>
+        <div className="">
+          <Suspense fallback={<div>Loading...</div>}>
             <FormStep2 />
-          </div>
+          </Suspense>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
