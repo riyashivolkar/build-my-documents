a438896560bdeb3422ad3250357ee91e70f56f88
Riya <64467020+riyashivolkar@users.noreply.github.com>
2024-11-11 08:24:14 +0530

    potato

--- src/app/talk-to-a-lawyer/booking/step-2/page.js
+++ src/app/talk-to-a-lawyer/booking/step-2/page.js
@@ -56,32 +56,34 @@ const Step2 = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
-        <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
-          <div className="bg-white ">
-            <Link href="/">
-              <Image
-                src="/logo1.png"
-                alt="logo"
-                width={280}
-                height={68}
-                priority
-                className="w-40 h-auto sm:w-80 md:h-auto"
-              />
-            </Link>
-          </div>
+    <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50">
+      <div className="relative w-full h-full bg-gray-100 rounded-lg shadow-lg">
+        <div className="bg-white ">
+          <Link href="/">
+            <Image
+              src="/logo1.png"
+              alt="logo"
+              width={280}
+              height={68}
+              priority
+              className="w-40 h-auto sm:w-80 md:h-auto"
+            />
+          </Link>
+        </div>
 
-          <div className="bg-gray-100 ">
+        <div className="bg-gray-100 ">
+          <Suspense fallback={<div>Loading Step2...</div>}>
             <StepStep2 />
-          </div>
+          </Suspense>
+        </div>
 
-          <div className="">
+        <div className="">
+          <Suspense fallback={<div>Loading Form Step2...</div>}>
             <FormStep2 />
-          </div>
+          </Suspense>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
