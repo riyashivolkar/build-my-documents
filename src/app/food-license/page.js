9e5ae3a1a38a04a509b2285582debfd87dc8216a
Riya <64467020+riyashivolkar@users.noreply.github.com>
2024-11-25 10:24:22 +0530

    chnaes

--- src/app/food-license/page.js
+++ src/app/food-license/page.js
@@ -1,124 +1,8 @@
-// import Link from "next/link";
-// import React from "react";
-
-// const Page = () => {
-//   const selectedService = "food";
-//   return (
-//     <section className="p-8 text-gray-500 body-font">
-//       <div className="px-24 mx-auto bg-white sm:px-5 py-14">
-//         <div className="flex flex-col w-full p-4 mb-20 text-start">
-//           <h1 className="pb-10  text-xl sm:text-4xl font-medium text-[#f7941d] title-font">
-//             Apply FSSAI Food Safety License Registration
-//           </h1>
-
-//           <ul className="mt-8 space-y-6">
-//             <li className="flex items-start space-x-4">
-//               <img
-//                 src="/document.svg"
-//                 alt="Description of image 1"
-//                 className="object-cover w-6 h-6 sm:w-12 sm:h-12"
-//               />
-//               <div>
-//                 <h2 className="sm:text-2xl text-md font-semibold text-[#f7941d]">
-//                   Documents Required Food License (Soft Copy)
-//                 </h2>
-//                 <p className="text-gray-500 text-md sm:text-2xl">
-//                   <br />
-//                   Aadhaar card
-//                   <br />
-//                   Pan Card
-//                   <br />
-//                   One Passport size photo
-//                   <br />
-//                   Fssai declaration form <br />
-//                   Nature of business details
-//                 </p>
-//               </div>
-//             </li>
-//             <br />
-//             <li className="flex items-start space-x-4">
-//               <img
-//                 src="/clock.svg"
-//                 alt="Description of image 1"
-//                 className="object-cover w-6 h-6 sm:w-12 sm:h-12"
-//               />
-//               <div>
-//                 <h2 className="sm:text-2xl text-md font-semibold text-[#f7941d]">
-//                   How it works{" "}
-//                 </h2>
-//                 <p className="text-gray-500 text-md sm:text-2xl">
-//                   <br />
-//                   Register Online
-//                   <br />
-//                   Upload Documents
-//                   <br />
-//                   Payment
-//                   <br />
-//                 </p>
-//               </div>
-//             </li>
-//             <br />
-//             <li className="flex items-start space-x-4">
-//               <img
-//                 src="/clock.svg"
-//                 alt="Description of image 1"
-//                 className="object-cover w-6 h-6 sm:w-12 sm:h-12"
-//               />
-//               <div>
-//                 <h2 className="sm:text-2xl text-md font-semibold text-[#f7941d]">
-//                   Time Duration
-//                 </h2>
-//                 <p className="text-gray-500 text-md sm:text-2xl">
-//                   <br />
-//                   1-2 working days
-//                   <br />
-//                 </p>
-//               </div>
-//             </li>
-//             <br />
-//             <li className="flex items-start space-x-4">
-//               <img
-//                 src="/money.svg"
-//                 alt="Description of image 1"
-//                 className="object-cover w-6 h-6 sm:w-12 sm:h-12"
-//               />
-//               <div>
-//                 <h2 className="sm:text-2xl text-md font-semibold text-[#f7941d]">
-//                   Charges
-//                 </h2>
-//                 <p className="text-gray-500 text-md sm:text-2xl">
-//                   <br />
-//                   Rs. 1690
-//                   <br />
-//                 </p>
-//               </div>
-//             </li>
-//             <br />
-//             <div className="flex flex-row items-end justify-end p-5 ">
-//               <Link href={`/form?service=${selectedService}`}>
-//                 <button className="bg-[#f7941d] px-4 py-2 sm:text-2xl text-sm shadow-lg text-white rounded-md">
-//                   CONTINUE
-//                 </button>
-//               </Link>
-//             </div>
-//           </ul>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Page;
-
 "use client";
 import React, { Suspense, useState } from "react";
-import FormLawyer from "../components/Talk-to-lawyer/LawyerForm";
 import ExpertModal from "../components/Talk-to-lawyer/ExpertModal";
-import ScrollGallery from "../components/Talk-to-lawyer/ScrollGallery";
 import Image from "next/image";
-import LawyerSteps from "../components/Talk-to-lawyer/LawyerSteps";
-import LawyerServices from "../components/Talk-to-lawyer/LawyerServices";
-import Reviews from "../components/Talk-to-lawyer/Reviews";
+
 import { legalExperts } from "../utils/data/talk2LawyerData";
 import FssaiBenifits from "../components/fssai/FssaiBenifits";
 import Steps from "../components/Steps";
@@ -150,8 +34,7 @@ const Page = () => {
       <div className="flex flex-wrap items-start px-5 py-16 mx-auto sm:py-24 sm:px-24">
         <div className="pr-0 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0">
           <h1 className="text-2xl font-bold text-gray-900 sm:text-6xl title-font">
-            Our Expert consultation will help you to get the food licence from
-            the Government{" "}
+            Our Expert consultation will help you to get the food licence
           </h1>
           <ul className="mt-12 space-y-4 text-base leading-relaxed sm:text-2xl">
             <li className="flex items-start">
